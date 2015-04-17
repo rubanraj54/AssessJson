3b8b782d2cf3734a4f763aa3b0888bf965dd52bb
rubanraj54 <rubanraj54@gmail.com>
2015-04-17 18:17:45 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,1 +1,9 @@
-
+angular.module('usingController', []).controller('collegeNames', function($scope) {
+    $scope.college = [
+		{name:'Anna university'},
+		{name:'Adhiyamaan'},
+		{name:'PSG Tech'},
+		{name:'Savitha'},
+		{name:'MEPCO'}
+    ];
+});
